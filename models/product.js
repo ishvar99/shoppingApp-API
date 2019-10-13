d80810bf6ff5fd9fae9a4609f46690d801bbb560
Ishan Varshney <ishanvarshney99@gmail.com>
2019-10-14 01:01:21 +0530

    bugs fixed

--- models/product.js
+++ models/product.js
@@ -1,5 +1,5 @@
 var mongoose=require('mongoose');
-mongoose.connect('localhost',{ useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/shop-app",{ useNewUrlParser: true });
 
 var productSchema=new mongoose.Schema({
     'title':String,
