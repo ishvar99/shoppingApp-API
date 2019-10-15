3e681afd010bd1b7e16ae488a37b474a89e07027
Ishan Varshney <ishanvarshney99@gmail.com>
2019-10-15 22:56:06 +0530

    changes reverted

--- app.js
+++ app.js
@@ -2,21 +2,12 @@ var express=require('express'),
     app=express();
 var bodyParser=require('body-parser')
 var shopRoutes=require('./routes/shop.js')
-var Product=require('./models/product.js');
 app.use(express.static(__dirname+"/public"))
 app.use(bodyParser.urlencoded({extended:true}))
 app.get('/',(req,res)=>{
 	res.sendFile('views/index.html',{root:__dirname});
 });
-app.get('/api/shop',(req,res)=>{
-	 Product.find({})
- .then((products)=>{
-      res.json(products);
- })
- .catch((err)=>{
- 	res.json({"error":err})
- })
-});
+app.use('/api/shop',shopRoutes);
 app.listen(process.env.PORT||'3000',process.env.IP,()=>{
 console.log('Server Started!')
 })
