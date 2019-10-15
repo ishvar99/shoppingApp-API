bba78b0266c7c4a55e35477971209c59e7cf2a0a
Ishan Varshney <ishanvarshney99@gmail.com>
2019-10-15 20:23:52 +0530

    bugs fixed

--- routes/shop.js
+++ routes/shop.js
@@ -3,20 +3,40 @@ var Product=require('../models/product.js');
 var router=express.Router();
 router.route('/')
 .get((req,res)=>{
- Product.find({},(err,products)=>{
- 	  if(err)
- 	  	res.json({
- 	  		"Error":err
- 	  	})
- 	  else
+ Product.find({})
+ .then((products)=>{
       res.json(products);
- });
+ })
+ .catch((err)=>{
+ 	res.json({"error":err})
+ })
 })
 .post((req,res)=>{
+	console.log(req.body);
    Product.create(
-   	req.body
-   ,(product)=>{
+   req.body
+   ).then((product)=>{
    	res.json(product)
    })
+   .catch((err)=>{
+   	console.log(err)
+   })
+})
+router.patch('/:id',(req,res)=>{
+    Product.findOneAndUpdate({_id:req.params.id},req.body,{new:true})
+    .then((foundProduct)=>{
+    	res.json(foundProduct)
+    })
+    .catch((err)=>{
+    	console.log(err)
+    })
+});
+router.delete('/:id',(req,res)=>{
+	Product.findOneAndRemove({_id:req.params.id})
+	.catch((err)=>{
+		console.log(err)
+	})
 })
 module.exports=router
+
+
