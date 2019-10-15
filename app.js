85b6a5a2230b1320ab8ca680d5834a975b999e8b
Ishan Varshney <ishanvarshney99@gmail.com>
2019-10-15 22:53:58 +0530

    bugs fixed

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ var express=require('express'),
     app=express();
 var bodyParser=require('body-parser')
 var shopRoutes=require('./routes/shop.js')
+var Product=require('./models/product.js');
 app.use(express.static(__dirname+"/public"))
 app.use(bodyParser.urlencoded({extended:true}))
 app.get('/',(req,res)=>{
 	res.sendFile('views/index.html',{root:__dirname});
 });
-app.use('/api/shop',shopRoutes);
+app.get('/api/shop',(req,res)=>{
+	 Product.find({})
+ .then((products)=>{
+      res.json(products);
+ })
+ .catch((err)=>{
+ 	res.json({"error":err})
+ })
+});
 app.listen(process.env.PORT||'3000',process.env.IP,()=>{
 console.log('Server Started!')
 })
